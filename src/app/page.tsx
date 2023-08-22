cb95eee985dafc4792e38b667b941d9b10ff8f00
Neville Kati <nevillecodes98@gmail.com>
2023-08-22 16:54:31 +0000

    add site title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from "next";
 import { HomePage } from "./home";
+
+export const metadata: Metadata = {
+	title: "ZooTools insights",
+	description:
+		"View statistics and details about your marketing campaigns with ZooTools",
+};
 
 export default function Home() {
 	return <HomePage />;
